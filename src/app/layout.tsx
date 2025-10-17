f9a3f0fea391a0f6daf6eaceb6d66dfb4fcd8b80
Tribes <sidharts894@gmail.com>
2025-10-17 13:33:17 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const switzer = localFont({
@@ -134,6 +135,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${switzer.variable} ${nohemi.variable}  antialiased`}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3XRZVZBTNJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3XRZVZBTNJ');
+          `}
+        </Script>
+        
         {children}
       </body>
     </html>
